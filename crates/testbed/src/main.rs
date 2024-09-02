4b805be8ddf85d9aec9d331aea5ef8de047f58de
WhyNeet <117515380+WhyNeet@users.noreply.github.com>
2024-09-02 18:50:31 +0300

    feat(testbed): update the example

--- crates/testbed/src/main.rs
+++ crates/testbed/src/main.rs
@@ -1,10 +1,23 @@
-use comet::mem::{TableEntity, TOTAL_DOCUMENT_SIZE};
+use comet::mem::{CollectionEntity, TOTAL_DOCUMENT_SIZE};
 
 fn main() {
-    let entity = TableEntity::new(0, "whyneet", "example@example.com");
+    let entity = CollectionEntity::new(0, "whyneet", "example@example.com");
+
+    println!("the original entity:");
+    entity.display();
 
     let mut output_buffer = [0u8; TOTAL_DOCUMENT_SIZE];
     entity.serialize(&mut output_buffer);
 
-    println!("output: {:?}", output_buffer);
+    let mut new_entity = CollectionEntity {
+        id: 0,
+        username: [0u8; 32],
+        email: [0u8; 255],
+    };
+
+    new_entity.deserialize(&output_buffer);
+
+    println!("the deserialized entity:");
+
+    new_entity.display();
 }
