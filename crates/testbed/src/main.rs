042dded18278e79d86ef3e33eb30a42266969a8f
WhyNeet <117515380+WhyNeet@users.noreply.github.com>
2024-09-02 21:15:24 +0300

    feat(testbed): update example

--- crates/testbed/src/main.rs
+++ crates/testbed/src/main.rs
@@ -5,8 +5,5 @@ fn main() {
 
     let document = Document::new(0, "whyneet", "example@example.com");
 
-    let slot = collection.create_document_slot();
-    collection.num_documents += 1;
-
-    document.serialize(unsafe { slot.as_mut().unwrap() });
+    collection.insert_document(&document);
 }
