b6cddc47152d39d55e02d7abbbc89cb94f8d72d2
WhyNeet <117515380+WhyNeet@users.noreply.github.com>
2024-09-02 22:53:07 +0300

    feat(testbed): update example

--- crates/testbed/src/main.rs
+++ crates/testbed/src/main.rs
@@ -3,7 +3,18 @@ use comet::mem::{Collection, Document};
 fn main() {
     let mut collection = Collection::new();
 
-    let document = Document::new(0, "whyneet", "example@example.com");
+    let document1 = Document::new(0, "whyneet", "example@example.com");
+    let document2 = Document::new(1, "test", "test@gmail.com");
 
-    collection.insert_document(&document);
+    collection.insert_document(&document1);
+    collection.insert_document(&document2);
+
+    let document_id = 1;
+    let stored_document = collection.retrieve_document(document_id);
+    if let Some(doc) = stored_document {
+        println!("Document stored with id: {document_id}");
+        doc.display();
+    } else {
+        println!("No document with id: {document_id}");
+    }
 }
