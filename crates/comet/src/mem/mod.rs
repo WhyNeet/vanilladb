dc182cf7842ce5f94154320ae47e788a77c9a9f8
WhyNeet <117515380+WhyNeet@users.noreply.github.com>
2024-09-02 17:45:29 +0300

    feat(comet): add method to create table entities

--- crates/comet/src/mem/mod.rs
+++ crates/comet/src/mem/mod.rs
@@ -7,18 +7,32 @@ pub const EMAIL_SIZE: usize = 255;
 
 pub const TOTAL_DOCUMENT_SIZE: usize = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
 
-pub struct TableEntity<'a> {
+pub struct TableEntity {
     pub id: u64,
-    pub username: &'a [u8; USERNAME_SIZE],
-    pub email: &'a [u8; EMAIL_SIZE],
+    pub username: [u8; USERNAME_SIZE],
+    pub email: [u8; EMAIL_SIZE],
 }
 
-impl TableEntity<'_> {
-    pub fn serialize(&mut self, dest: &mut [u8]) {
+impl TableEntity {
+    pub fn new(id: u64, username: &str, email: &str) -> Self {
+        let mut username_bytes = [0u8; USERNAME_SIZE];
+        let mut email_bytes = [0u8; EMAIL_SIZE];
+
+        unsafe { TableEntity::write_to_buffer(username.as_bytes(), &mut username_bytes, 0) };
+        unsafe { TableEntity::write_to_buffer(email.as_bytes(), &mut email_bytes, 0) };
+
+        TableEntity {
+            id,
+            username: username_bytes,
+            email: email_bytes,
+        }
+    }
+
+    pub fn serialize(&self, dest: &mut [u8]) {
         let id = self.id.to_ne_bytes();
         unsafe { TableEntity::write_to_buffer(&id, dest, 0) };
-        unsafe { TableEntity::write_to_buffer(self.username, dest, ID_SIZE) }
-        unsafe { TableEntity::write_to_buffer(self.email, dest, ID_SIZE + USERNAME_SIZE) }
+        unsafe { TableEntity::write_to_buffer(&self.username, dest, ID_SIZE) }
+        unsafe { TableEntity::write_to_buffer(&self.email, dest, ID_SIZE + USERNAME_SIZE) }
     }
 
     unsafe fn write_to_buffer(from: &[u8], to: &mut [u8], offset: usize) {
