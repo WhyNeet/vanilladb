405b3574d0f68ecd770936bb601ce0aea57804a8
WhyNeet <117515380+WhyNeet@users.noreply.github.com>
2024-09-04 15:40:42 +0300

    feat(testbed): update example

--- crates/testbed/src/main.rs
+++ crates/testbed/src/main.rs
@@ -1,11 +1,18 @@
 use comet::mem::{Comet, Document};
 
 fn main() {
+    println!("--- writing data ---");
+    write_data();
+
+    println!("--- reading data ---");
+    read_data();
+}
+
+fn write_data() {
     let mut comet = Comet::new(".comet_data".to_string());
     comet.initialize().unwrap();
 
     let database = comet.create_database("primary".to_string());
-
     let collection = database.create_collection("users".to_string());
 
     let document1 = Document::new(0, "whyneet", "example@example.com");
@@ -13,6 +20,14 @@ fn main() {
 
     collection.insert_document(&document1);
     collection.insert_document(&document2);
+
+    comet.flush().unwrap();
+}
+
+fn read_data() {
+    let mut comet = Comet::new(".comet_data".to_string());
+    comet.initialize().unwrap();
+    comet.load().unwrap();
 
     // let document_id = 1;
     // let stored_document = collection.retrieve_document(document_id);
@@ -22,6 +37,4 @@ fn main() {
     // } else {
     //     println!("No document with id: {document_id}");
     // }
-
-    comet.flush().unwrap();
 }
