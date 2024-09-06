edfb72c6c1c19556365a6ce4c24074403b06e2e7
WhyNeet <117515380+WhyNeet@users.noreply.github.com>
2024-09-06 19:06:07 +0300

    refactor(ccb): remove serialization of types with platform-dependent size

--- crates/ccb/src/deserialize/implementation.rs
+++ crates/ccb/src/deserialize/implementation.rs
@@ -1,6 +1,6 @@
 use super::macro_impl::deserializable_number;
 
-deserializable_number!(for usize, u128, u64, u32, u16, u8, isize, i128, i64, i32, i16, i8, f64, f32);
+deserializable_number!(for u128, u64, u32, u16, u8, i128, i64, i32, i16, i8, f64, f32);
 
 impl Deserialize for String {
     fn deserialize(from: Box<[u8]>) -> Result<Self, Box<dyn Error>> {
--- crates/ccb/src/serialize/implementation.rs
+++ crates/ccb/src/serialize/implementation.rs
@@ -2,7 +2,7 @@ use std::mem;
 
 use super::macro_impl::serializable_number;
 
-serializable_number!(for usize, u128, u64, u32, u16, u8, isize, i128, i64, i32, i16, i8, f64, f32);
+serializable_number!(for u128, u64, u32, u16, u8, i128, i64, i32, i16, i8, f64, f32);
 
 impl Serialize for String {
     fn serialize(self) -> Result<Box<[u8]>, Box<dyn Error>> {
