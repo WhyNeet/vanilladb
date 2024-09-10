83298645ef02fc14ff556c4d9e4a984099af1d49
WhyNeet <117515380+WhyNeet@users.noreply.github.com>
2024-09-10 22:34:13 +0300

    feat(testbed): update example

--- crates/testbed/src/main.rs
+++ crates/testbed/src/main.rs
@@ -1,23 +1,19 @@
 use std::io;
 
-use comet::{
-    comet::Comet,
-    document::Document,
-    io::{comet_io::CometIo, io_config::IOConfig},
-};
+use comet::{comet::Comet, document::Document, io::io_config::IoConfig};
 use trail::field::Field;
 
 fn main() -> io::Result<()> {
     println!("--- writing data ---");
 
-    let config = IOConfig::builder()
+    let config = IoConfig::builder()
         .data_dir(".comet_data".to_string())
         .build();
-    let mut comet = Comet::new(CometIo::new(config));
+    let mut comet = Comet::new(config);
     comet.initialize().unwrap();
 
     let database = comet.create_database("primary".to_string())?;
-    let collection = database.create_collection("users".to_string())?;
+    let collection = database.create_collection("users".to_string()).unwrap();
 
     for i in 0..1000 {
         let mut document = Document::new();
@@ -35,45 +31,3 @@ fn main() -> io::Result<()> {
 
     Ok(())
 }
-
-// fn write_data() {
-//     let config = IOConfig::builder()
-//         .data_dir(".comet_data".to_string())
-//         .build();
-//     let mut comet = Comet::new(DirectIO::new(config));
-//     comet.initialize().unwrap();
-
-//     let database = comet.create_database("primary".to_string());
-//     let collection = database.create_collection("users".to_string());
-
-//     for i in 0..1000 {
-//         collection.insert_document(&Document::new(
-//             i,
-//             &format!("user {i}"),
-//             &format!("user{i}@example.com"),
-//         ))
-//     }
-
-//     comet.flush().unwrap();
-// }
-
-// fn read_data() {
-//     let config = IOConfig::builder()
-//         .data_dir(".comet_data".to_string())
-//         .build();
-//     let mut comet = Comet::new(DirectIO::new(config));
-//     comet.initialize().unwrap();
-//     comet.load().unwrap();
-
-//     let database = comet.database("primary").unwrap();
-//     let collection = database.collection("users").unwrap();
-
-//     let document_id = 1;
-//     let stored_document = collection.retrieve_document(document_id);
-//     if let Some(doc) = stored_document {
-//         println!("Document stored with id: {document_id}");
-//         doc.display();
-//     } else {
-//         println!("No document with id: {document_id}");
-//     }
-// }
