3ef0643e478b18f7c0b53cab29b4bb001ffc1e7e
WhyNeet <117515380+WhyNeet@users.noreply.github.com>
2024-09-14 21:24:54 +0300

    feat(testbed): update example

--- crates/testbed/src/main.rs
+++ crates/testbed/src/main.rs
@@ -1,9 +1,43 @@
-use std::io;
+use std::{error::Error, path::PathBuf};
 
 use comet::{comet::Comet, document::Document, io::io_config::IoConfig};
 use trail::field::Field;
 
-fn main() -> io::Result<()> {
+fn main() {
+    if PathBuf::from(".comet_data").exists() {
+        read().unwrap();
+    } else {
+        write().unwrap();
+    }
+}
+
+fn read() -> Result<(), Box<dyn Error>> {
+    println!("--- reading data ---");
+
+    let config = IoConfig::builder()
+        .data_dir(".comet_data".to_string())
+        .build();
+    let mut comet = Comet::new(config);
+    comet.initialize().unwrap();
+
+    let database = comet.create_database("primary".to_string())?;
+    let collection = database.create_collection("users".to_string()).unwrap();
+
+    let mut cursor = collection.cursor();
+
+    for _ in 0..50 {
+        cursor.next_document()?;
+    }
+
+    let document = cursor.read_current_document()?;
+    println!("document: {document:?}");
+
+    println!("--- done ---");
+
+    Ok(())
+}
+
+fn write() -> Result<(), Box<dyn Error>> {
     println!("--- writing data ---");
 
     let config = IoConfig::builder()
