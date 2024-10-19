f318ce9ce111f28d2a16059ab95f5054517f9dfa
WhyNeet <117515380+WhyNeet@users.noreply.github.com>
2024-10-19 21:36:21 +0300

    feat(llio): add record id

--- crates/llio/src/lib.rs
+++ crates/llio/src/lib.rs
@@ -1,3 +1,4 @@
 pub mod io;
 pub mod page;
 pub mod pager;
+pub mod util;
---
+++ crates/llio/src/util/record_id.rs
@@ -0,0 +1,18 @@
+pub struct RecordId {
+    path: String,
+    offset: u64,
+}
+
+impl RecordId {
+    pub fn new(path: String, offset: u64) -> Self {
+        Self { path, offset }
+    }
+
+    pub fn path(&self) -> &str {
+        &self.path
+    }
+
+    pub fn offset(&self) -> u64 {
+        self.offset
+    }
+}
