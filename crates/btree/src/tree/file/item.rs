21b84151f8dd4f3d3226b3b1fedfe852c5d0b42d
WhyNeet <117515380+WhyNeet@users.noreply.github.com>
2024-10-23 21:56:22 +0300

    feat(btree): add node item serialization

--- crates/btree/src/tree/file/item.rs
+++ crates/btree/src/tree/file/item.rs
@@ -1,16 +1,17 @@
 use std::rc::Rc;
+use std::{mem, ptr};
 
 use llio::util::record_id::RecordId;
 use trail::serialize::Serialize;
 
 #[derive(Debug)]
-pub enum FileBTreeNodeItem<Key: Clone, Value: Serialize> {
+pub enum FileBTreeNodeItem<Key: Clone + Serialize, Value: Serialize> {
     Key(Key),
     Pair(Key, Vec<Rc<Value>>),
     Pointer(RecordId),
 }
 
-impl<Key: Clone, Value: Serialize> FileBTreeNodeItem<Key, Value> {
+impl<Key: Clone + Serialize, Value: Serialize> FileBTreeNodeItem<Key, Value> {
     pub fn as_pair(&self) -> (&Key, &[Rc<Value>]) {
         match self {
             FileBTreeNodeItem::Pair(k, v) => (k, v),
@@ -68,5 +69,87 @@ impl<Key: Clone, Value: Serialize> FileBTreeNodeItem<Key, Value> {
             }
             _ => unreachable!(),
         }
+    }
+}
+
+impl<Key, Value> Serialize for FileBTreeNodeItem<Key, Value>
+where
+    Key: Clone + Serialize,
+    Value: Serialize,
+{
+    fn size(&self) -> u32 {
+        // type + size + item
+        mem::size_of::<u8>() as u32
+            + mem::size_of::<u32>() as u32
+            + match self {
+                Self::Key(key) => key.size(),
+                // key length + \0 + vector size bytes + vector size
+                Self::Pair(key, value) => {
+                    key.size() + 1 + mem::size_of::<u32>() as u32 + value.size()
+                }
+                Self::Pointer(rci) => rci.size(),
+            }
+    }
+
+    fn serialize(&self) -> Result<Box<[u8]>, Box<dyn std::error::Error>> {
+        let size = self.size();
+        let mut buffer = vec![0u8; size as usize].into_boxed_slice();
+
+        match self {
+            Self::Key(key) => {
+                buffer[0] = 0;
+
+                unsafe {
+                    ptr::copy_nonoverlapping(
+                        key.serialize()?.as_ptr(),
+                        buffer.as_mut_ptr().add(1),
+                        size as usize,
+                    );
+                };
+            }
+            Self::Pair(key, value) => {
+                buffer[0] = 1;
+
+                let key_size = key.size();
+                unsafe {
+                    ptr::copy_nonoverlapping(
+                        key.serialize()?.as_ptr(),
+                        buffer.as_mut_ptr().add(1),
+                        key_size as usize,
+                    );
+                }
+
+                let value_size = value.size();
+                unsafe {
+                    ptr::copy_nonoverlapping(
+                        value_size.to_le_bytes().as_ptr(),
+                        (&mut buffer[(1 + key_size as usize + 1)..]).as_mut_ptr(),
+                        mem::size_of::<u32>(),
+                    );
+                }
+
+                unsafe {
+                    ptr::copy_nonoverlapping(
+                        value.serialize()?.as_ptr(),
+                        (&mut buffer[(1 + key_size as usize + 1 + mem::size_of::<u32>())..])
+                            .as_mut_ptr(),
+                        value_size as usize,
+                    );
+                }
+            }
+            Self::Pointer(ptr) => {
+                buffer[0] = 2;
+
+                unsafe {
+                    ptr::copy_nonoverlapping(
+                        ptr.serialize()?.as_ptr(),
+                        buffer.as_mut_ptr().add(1),
+                        ptr.size() as usize,
+                    );
+                };
+            }
+        };
+
+        Ok(buffer)
     }
 }
