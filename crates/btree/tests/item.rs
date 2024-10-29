ab7017f5c5c97d256eb3adf2572dc531146d916c
WhyNeet <117515380+WhyNeet@users.noreply.github.com>
2024-10-29 13:53:57 +0300

    test(btree): add pointer item serialization test

--- crates/btree/tests/item.rs
+++ crates/btree/tests/item.rs
@@ -1,6 +1,7 @@
 use std::rc::Rc;
 
 use btree::tree::file::item::FileBTreeNodeItem;
+use llio::util::record_id::RecordId;
 use trail::{field::Field, serialize::Serialize};
 
 #[test]
@@ -38,4 +39,22 @@ pub fn item_pair_serialization_works() {
             2, 0, 0, 0, 76, 65
         ]
     )
+}
+
+#[test]
+pub fn item_pointer_serialization_works() {
+    let item: FileBTreeNodeItem<String> =
+        FileBTreeNodeItem::Pointer(RecordId::new("/hello/world".to_string(), 512));
+
+    let buffer = item.serialize();
+    assert!(buffer.is_ok());
+
+    let buffer = buffer.unwrap();
+    assert_eq!(
+        &buffer[..],
+        [
+            2, 22, 0, 0, 0, 12, 0, 47, 104, 101, 108, 108, 111, 47, 119, 111, 114, 108, 100, 0, 2,
+            0, 0, 0, 0, 0, 0
+        ]
+    );
 }
