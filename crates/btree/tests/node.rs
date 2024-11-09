06eefa5267f839d4ac22d604f9097bff72753c5d
WhyNeet <117515380+WhyNeet@users.noreply.github.com>
2024-11-09 20:41:52 +0300

    test(btree): update node ser/de tests

--- crates/btree/tests/node.rs
+++ crates/btree/tests/node.rs
@@ -5,10 +5,10 @@ use trail::{deserialize::Deserialize, field::Field, serialize::Serialize};
 
 #[test]
 pub fn node_serialization_works() {
-    let mut node = FileBTreeNode::<u32, String>::empty(false);
-    node.append(FileBTreeNodeItem::Key(10));
+    let mut node = FileBTreeNode::empty(false);
+    node.append(FileBTreeNodeItem::Key(Rc::new(Field::ubyte(10))));
     node.append(FileBTreeNodeItem::Pair(
-        11,
+        Rc::new(Field::ubyte(11)),
         vec![Rc::new(Field::string("value".to_string()))],
     ));
 
@@ -19,8 +19,8 @@ pub fn node_serialization_works() {
     assert_eq!(
         &buffer[..],
         [
-            37, 0, 0, 0, 0, 0, 4, 0, 0, 0, 10, 0, 0, 0, 1, 18, 0, 0, 0, 4, 0, 0, 0, 11, 0, 0, 0, 0,
-            5, 0, 0, 0, 118, 97, 108, 117, 101
+            33, 0, 0, 0, 0, 0, 2, 1, 0, 0, 0, 10, 1, 20, 0, 0, 0, 2, 1, 0, 0, 0, 11, 0, 5, 0, 0, 0,
+            118, 97, 108, 117, 101
         ]
     );
 }
@@ -28,20 +28,37 @@ pub fn node_serialization_works() {
 #[test]
 pub fn node_deserialization_works() {
     let buffer = [
-        37, 0, 0, 0, 0, 0, 4, 0, 0, 0, 10, 0, 0, 0, 1, 18, 0, 0, 0, 4, 0, 0, 0, 11, 0, 0, 0, 0, 5,
-        0, 0, 0, 118, 97, 108, 117, 101,
+        33, 0, 0, 0, 0, 0, 2, 1, 0, 0, 0, 10, 1, 20, 0, 0, 0, 2, 1, 0, 0, 0, 11, 0, 5, 0, 0, 0,
+        118, 97, 108, 117, 101,
     ];
-    let node = FileBTreeNode::<u32, String>::deserialize(&buffer);
+    let node = FileBTreeNode::deserialize(&buffer);
     assert!(node.is_ok());
 
     let node = node.unwrap();
     assert_eq!(node.is_internal(), false);
 
     assert!(node.items()[0].is_key());
-    assert_eq!(node.items()[0].as_key(), &10);
+    assert_eq!(
+        unsafe {
+            (Box::into_raw(ptr::read(node.items()[0].as_key().value() as *const Box<_>))
+                as *const u8)
+                .as_ref()
+                .unwrap()
+        },
+        &10
+    );
 
     assert!(node.items()[1].is_pair());
-    assert_eq!(node.items()[1].as_pair().0, &11);
+    assert_eq!(
+        unsafe {
+            (Box::into_raw(ptr::read(
+                node.items()[1].as_pair().0.value() as *const Box<_>
+            )) as *const u8)
+                .as_ref()
+                .unwrap()
+        },
+        &11
+    );
     assert_eq!(
         unsafe {
             (Box::into_raw(ptr::read(
