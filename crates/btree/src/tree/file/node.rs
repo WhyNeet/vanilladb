c1f6d7619b9d3e16e35bc8025d67c51541d39e57
WhyNeet <117515380+WhyNeet@users.noreply.github.com>
2024-11-17 21:03:47 +0300

    refactor(btree): only use record ids as node children

--- crates/btree/src/tree/file/node.rs
+++ crates/btree/src/tree/file/node.rs
@@ -1,14 +1,13 @@
 use std::{mem, ptr};
 
+use llio::util::record_id::RecordId;
 use trail::{deserialize::Deserialize, serialize::Serialize};
-
-use super::item::FileBTreeNodeItem;
 
 #[derive(Debug)]
 pub struct FileBTreeNode {
-    items: Vec<FileBTreeNodeItem>,
+    items: Vec<RecordId>,
     internal: bool,
-    non_ptr_items: usize,
+    // non_ptr_items: usize,
 }
 
 impl FileBTreeNode {
@@ -16,17 +15,17 @@ impl FileBTreeNode {
         Self {
             items: Vec::new(),
             internal,
-            non_ptr_items: 0,
+            // non_ptr_items: 0,
         }
     }
 
-    pub fn from_items(items: &[FileBTreeNodeItem]) -> Self {
+    pub fn from_items(items: &[RecordId]) -> Self {
         let mut node = Self::empty(false);
         for it in items {
-            node.append(it.cloned());
-            if !it.is_pair() {
-                node.set_internal(true);
-            }
+            node.append(it.clone());
+            // if !it.is_pair() {
+            //     node.set_internal(true);
+            // }
         }
 
         node
@@ -36,47 +35,47 @@ impl FileBTreeNode {
         self.internal = internal;
     }
 
-    pub fn append(&mut self, item: FileBTreeNodeItem) {
-        if !item.is_pointer() {
-            self.non_ptr_items += 1
-        }
+    pub fn append(&mut self, item: RecordId) {
+        // if !item.is_pointer() {
+        //     self.non_ptr_items += 1
+        // }
         self.items.push(item);
     }
 
-    pub fn pop(&mut self) -> Option<FileBTreeNodeItem> {
+    pub fn pop(&mut self) -> Option<RecordId> {
         self.items.pop()
     }
 
-    pub fn insert(&mut self, item: FileBTreeNodeItem, idx: usize) {
-        if !item.is_pointer() {
-            self.non_ptr_items += 1
-        }
+    pub fn insert(&mut self, item: RecordId, idx: usize) {
+        // if !item.is_pointer() {
+        //     self.non_ptr_items += 1
+        // }
         self.items.splice(idx..idx, [item]);
     }
 
-    pub fn replace(&mut self, item: FileBTreeNodeItem, idx: usize) -> Option<FileBTreeNodeItem> {
-        if !item.is_pointer() {
-            self.non_ptr_items += 1
-        }
+    pub fn replace(&mut self, item: RecordId, idx: usize) -> Option<RecordId> {
+        // if !item.is_pointer() {
+        //     self.non_ptr_items += 1
+        // }
         if self.items.get(idx).is_none() {
             return None;
         }
-        if self.items.get(idx).unwrap().is_pointer() {
-            self.non_ptr_items -= 1
-        }
+        // if self.items.get(idx).unwrap().is_pointer() {
+        //     self.non_ptr_items -= 1
+        // }
 
         Some(std::mem::replace(&mut self.items[idx], item))
     }
 
-    pub fn get(&self, idx: usize) -> Option<&FileBTreeNodeItem> {
+    pub fn get(&self, idx: usize) -> Option<&RecordId> {
         self.items.get(idx)
     }
 
-    pub fn last(&self) -> Option<&FileBTreeNodeItem> {
+    pub fn last(&self) -> Option<&RecordId> {
         self.items.last()
     }
 
-    pub fn items(&self) -> &[FileBTreeNodeItem] {
+    pub fn items(&self) -> &[RecordId] {
         &self.items
     }
 
@@ -84,9 +83,9 @@ impl FileBTreeNode {
         self.internal
     }
 
-    pub fn non_ptr_len(&self) -> usize {
-        self.non_ptr_items
-    }
+    // pub fn non_ptr_len(&self) -> usize {
+    //     self.non_ptr_items
+    // }
 }
 
 impl Serialize for FileBTreeNode {
@@ -146,7 +145,7 @@ impl Deserialize for FileBTreeNode {
         let mut offset = mem::size_of::<u32>() + mem::size_of::<bool>();
 
         while offset < size as usize {
-            let item = FileBTreeNodeItem::deserialize(&from[offset..])?;
+            let item = RecordId::deserialize(&from[offset..])?;
             offset += item.size() as usize;
 
             node.append(item);
