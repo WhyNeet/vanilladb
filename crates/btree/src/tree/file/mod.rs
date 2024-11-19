2b77f21676d34d81f7c02f644833b3d708bd8af4
WhyNeet <117515380+WhyNeet@users.noreply.github.com>
2024-11-19 22:14:02 +0300

    feat(btree): automatically create a root if it doesnt exist

--- crates/btree/src/tree/file/mod.rs
+++ crates/btree/src/tree/file/mod.rs
@@ -1,16 +1,15 @@
 pub mod item;
 pub mod node;
 
-use std::{error::Error, io::Read, mem};
-
-use llio::{
-    io::direct::DirectFileIo,
-    page::PAGE_SIZE,
-    pager::Pager,
-    util::record_id::{self, RecordId},
+use std::{
+    error::Error,
+    io::{Read, Write},
+    mem,
 };
+
+use llio::{io::direct::DirectFileIo, page::PAGE_SIZE, pager::Pager, util::record_id::RecordId};
 use node::FileBTreeNode;
-use trail::{deserialize::Deserialize, field::FieldType};
+use trail::{deserialize::Deserialize, field::FieldType, serialize::Serialize};
 
 /// A file-based B+ tree
 pub struct FileBTree {
@@ -41,18 +40,36 @@ impl FileBTree {
 }
 
 impl FileBTree {
-    fn root_rci(&self) -> Result<RecordId, Box<dyn Error>> {
+    fn root_rci(&self) -> Result<Option<RecordId>, Box<dyn Error>> {
         let mut root_rci_len = vec![0u8; mem::size_of::<u32>()];
-        let mut page = self.metadata.load_page(0).unwrap();
+        let mut page = self.metadata.load_page(0)?;
 
         page.read(&mut root_rci_len)?;
         let root_rci_len = u32::deserialize(&root_rci_len)?;
 
+        if root_rci_len == 0 {
+            return Ok(None);
+        }
+
         let mut root_rci = vec![0u8; root_rci_len as usize];
-        page.read_at(&mut root_rci, mem::size_of::<u32>() as u16)?;
+        page.read(&mut root_rci)?;
         let root_rci = RecordId::deserialize(&root_rci)?;
 
-        Ok(root_rci)
+        Ok(Some(root_rci))
+    }
+
+    fn create_root(&mut self) -> Result<(FileBTreeNode, RecordId), Box<dyn Error>> {
+        let root = FileBTreeNode::empty(false);
+
+        self.pager.write(&root.serialize()?)?;
+
+        let root_record_id = RecordId::new("".to_string(), 0);
+
+        let mut metadata_page = self.metadata.load_page(0)?;
+        metadata_page.write(&root_record_id.serialize()?)?;
+        self.metadata.flush_page(0, metadata_page)?;
+
+        Ok((root, root_record_id))
     }
 
     pub fn key_type(&self) -> Result<FieldType, Box<dyn Error>> {
@@ -65,22 +82,25 @@ impl FileBTree {
         Ok(key_type)
     }
 
-    pub fn root(&self) -> Result<FileBTreeNode, Box<dyn Error>> {
-        let root_rci = self.root_rci()?;
-        let offset = root_rci.offset();
-        let page_idx = offset / PAGE_SIZE as u64;
-        let page_offset = offset % PAGE_SIZE as u64;
+    pub fn root(&mut self) -> Result<FileBTreeNode, Box<dyn Error>> {
+        if let Some(root_rci) = self.root_rci()? {
+            let offset = root_rci.offset();
+            let page_idx = offset / PAGE_SIZE as u64;
+            let page_offset = offset % PAGE_SIZE as u64;
 
-        let mut size = vec![0u8; mem::size_of::<u32>()].into_boxed_slice();
-        self.pager
-            .read_at(&mut size, (page_idx, page_offset as u16))?;
-        let size = u32::deserialize(&size)?;
-        let mut root = vec![0u8; size as usize].into_boxed_slice();
-        self.pager
-            .read_at(&mut root, (page_idx, page_offset as u16))?;
-        let root = FileBTreeNode::deserialize(&root)?;
+            let mut size = vec![0u8; mem::size_of::<u32>()].into_boxed_slice();
+            self.pager
+                .read_at(&mut size, (page_idx, 2 + page_offset as u16))?;
+            let size = u32::deserialize(&size)?;
+            let mut root = vec![0u8; size as usize].into_boxed_slice();
+            self.pager
+                .read_at(&mut root, (page_idx, 2 + page_offset as u16))?;
+            let root = FileBTreeNode::deserialize(&root)?;
 
-        Ok(root)
+            Ok(root)
+        } else {
+            Ok(self.create_root()?.0)
+        }
     }
 
     fn get_node_child(
